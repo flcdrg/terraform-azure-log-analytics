8121313de329605eadaf4da1aab40812e3c7a7dc
David Gardiner <david@gardiner.net.au>
2023-08-11 14:37:23 +0930

    Fix replication type

--- storage.tf
+++ storage.tf
@@ -4,7 +4,7 @@ resource "azurerm_storage_account" "storage" {
   resource_group_name             = data.azurerm_resource_group.group.name
   access_tier                     = "Cool"
   account_kind                    = "StorageV2"
-  account_replication_type        = "ZRS"
+  account_replication_type        = "LRS"
   account_tier                    = "Standard"
   allow_nested_items_to_be_public = false
   enable_https_traffic_only       = true
